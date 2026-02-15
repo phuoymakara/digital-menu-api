2137acc4bb6e340d82ebe7af787db5fdee292ced
mark <phuoymakara@gmail.com>
2026-02-15 19:39:23 +0700

    fix error

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -20,7 +20,7 @@ import { AuthModule } from '../modules/auth/auth.module';
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
-      envFilePath: ['.env'],
+      // envFilePath: ['.env'],
     }),
     TypeOrmModule.forRootAsync({
       inject: [ConfigService],
